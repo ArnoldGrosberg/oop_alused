ad241fe23b9a6211e6ce5753e7fdd043259dd1f4
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-04 14:45:13 +0200

    Kasutaja sisend ning tüübiteisendus - ülesanne 1.3

--- src/test.java
+++ src/test.java
@@ -1,15 +1,24 @@
 /*
- * Ül 1.2
+ * Ül 1.3
  *
  * */
 
+import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
-        int aasta = 2020;
-        String liblikas = "teelehe-mosaiikliblikas";
-        String lause_keskosa = ". aasta liblikas on ";
-        String lause = aasta + lause_keskosa + liblikas;
-        System.out.println(lause);
-
+        // defineerime muutujad2
+        // lubame ksutajale sisestada midagi
+        Scanner sisesnd = new Scanner(System.in);
+        // selvestame kasutaja sisestatud väärtus
+        System.out.println("Sisestage astme alus: ");
+        int astmeAlus = sisesnd.nextInt();
+        // selgitame kasutajale, mida tuleb sisestada
+        System.out.println("Sisestage astendaja: ");
+        // salvestame ksutaja sisestatud väärtus
+        int astendaja = sisesnd.nextInt();
+        // arvutamine
+        int tulemus = (int) Math.pow(astmeAlus, astendaja);
+        // väljastamine
+        System.out.println(tulemus);
     }
 }
