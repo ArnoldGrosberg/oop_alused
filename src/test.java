d8fa3bfcd1b06a5f0941ec13ac356dc27b9267a5
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-08 14:10:38 +0200

    Tervitused mõtisklustega - ülesanne 6.3a

--- src/test.java
+++ src/test.java
@@ -1,31 +1,38 @@
 /*
- * Ül 6.3
+ * Ül 6.3a
  * */
 import java.util.Scanner;
 
 public class test {
     // meetod
-    static double eelarve(double kulalistearv) {
-        double number = (55 + kulalistearv * 10);
-        return number;
+    static String tervitus(int kord) {
+        String kordStr = Integer.toString(kord);
+        kordStr = ("Võõrustaja: \"Tere!\"\n" +
+                "Täna " + kordStr + ". kord tervitada, mõtiskleb võõrustaja.\n" +
+                "Külaline: \"Tere, suur tänu kutse eest!\"");
+        return kordStr;
     }
 
     //main meetod
     public static void main(String[] args) {
+        Scanner sisend = new Scanner(System.in);
         // defineerime muutujad
         // selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Mitu inimest on kutsutud? ");
+        System.out.println("Sisestage külaliste arv: ");
         //lubame kasutajale sisestada midagi
-        Scanner sisend = new Scanner(System.in);
+        int külaliste_arv = sisend.nextInt();
         // salvestame kasutaja sisestatud väärtus
-        double kutsutud = sisend.nextDouble();
-        System.out.println("Mitu inimest tuleb? ");
-        //lubame kasutajale sisestada midagi
-        sisend = new Scanner(System.in);
-        // salvestame kasutaja sisestatud väärtus
-        double kulalistearv = sisend.nextDouble();
-        // arvutamine meetotiga
-        System.out.println("Max: " + eelarve(kutsutud) + "€");
-        System.out.println("Min: " + eelarve(kulalistearv) + "€");
+        // väljastamine
+        int kord = 1;
+        while (külaliste_arv >= kord && külaliste_arv < 4) {
+            System.out.println(tervitus(kord));
+            kord++;
+        }
+        while (3 >= kord & külaliste_arv > 4) {
+            System.out.println(tervitus(kord));
+            kord++;
+        }
+
+
     }
 }
