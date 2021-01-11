317cbafcd2c14419561c2e13f7db9f4eebcbe22c
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-11 15:49:27 +0200

    ül 5.1 Ülikooli vastuvõetud

--- src/test.java
+++ src/test.java
@@ -1,86 +1,39 @@
 /*
- * Ül 5.1
+ * massiivid
+ * ülesanne 5.1
  * */
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.io.IOException;
-
-public class test {
-    public static void main(String[] args) {
-        try {
-            System.out.println("Katsetamine failist lugemine.");
-            File fail = new File("C:\\Users\\arnold\\IdeaProjects\\oop_alused\\src\\vastuvoetud.txt");
-            Scanner sisendFailist = new Scanner(fail);
-            while (sisendFailist.hasNextLine()) {
-                String data = sisendFailist.nextLine();
-                System.out.println(data);
-            }
-            sisendFailist.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Faili pole.");
-            e.printStackTrace();
-        }
-    }
-}
-
-/*
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class ReadFile {
-    public static void main(String[] args) {
-        try {
-            File myObj = new File("filename.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-}
-*/
-/*
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
-    // meetod
-    static String tervitus(int kord) {
-        String kordStr = Integer.toString(kord);
-        kordStr = ("Võõrustaja: \"Tere!\"\n" +
-                "Täna " + kordStr + ". kord tervitada, mõtiskleb võõrustaja.\n" +
-                "Külaline: \"Tere, suur tänu kutse eest!\"");
-        return kordStr;
-    }
-
-    //main meetod
     public static void main(String[] args) {
-        Scanner sisend = new Scanner(System.in);
-        // defineerime muutujad
-        // selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Sisestage külaliste arv: ");
-        //lubame kasutajale sisestada midagi
-        int külaliste_arv = sisend.nextInt();
-        // salvestame kasutaja sisestatud väärtus
-        // väljastamine
-        int kord = 1;
-        while (külaliste_arv >= kord && külaliste_arv < 4) {
-            System.out.println(tervitus(kord));
-            kord++;
+        // massiiv faili sisu hoidmiseks
+        ArrayList<String> aastad = new ArrayList<>();
+        // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
+        File fail = new File("C:\\Users\\anna\\IdeaProjects\\oop_alused\\src\\vastuvoetud.txt");
+        Scanner sisendFailist = null;
+        try {
+            sisendFailist = new Scanner(fail);
+        } catch (Exception e) {
+            System.out.println("Faili pole - " + e.getMessage());
         }
-        while (3 >= kord & külaliste_arv > 4) {
-            System.out.println(tervitus(kord));
-            kord++;
+        // loeme failist
+        while (sisendFailist.hasNextLine()) {
+            String rida = sisendFailist.nextLine();
+            aastad.add(rida); // lisame loetud väärtus nimekirja sisse
         }
-
-
+        sisendFailist.close();
+        // vaatame nimekirja sisu
+        for (int i = 0; i < aastad.size(); i++) {
+            System.out.println(aastad.get(i));
+        }
+        // küsime andmed kasutaja käest
+        Scanner sisendKasutajalt = new Scanner(System.in);
+        System.out.println("Sisesta aasta: ");
+        int aasta = sisendKasutajalt.nextInt();
+        sisendKasutajalt.close();
+        System.out.println("2011 aastas vastu võetud " + aastad.get(aasta - 2011));
     }
 }
-*/
