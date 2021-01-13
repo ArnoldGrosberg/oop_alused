acd0ecdab3c50bb2169f3dcc5969fce648d18ed9
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-13 13:02:48 +0200

    Ül 5.4C Tahvlijuurde

--- src/test.java
+++ src/test.java
@@ -1,19 +1,20 @@
 /*
  * massiivid
- * ülesanne 5.4a Jukebox
+ * ülesanne 5.4c Tahvlijuurde
  * */
+
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner sisesnd = new Scanner(System.in);
-        System.out.println("Palun sisestage failinimi: ");
-        String koht = sisesnd.nextLine();
         // massiiv faili sisu hoidmisekskont
-        ArrayList<String> kontosisu = new ArrayList<>();
+        ArrayList<String> opilased = new ArrayList<>();
         // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
-        File fail = new File("C:\\Users\\arnold\\IdeaProjects\\oop_alused\\src\\" + koht);
+        File fail = new File("C:\\Users\\arnold\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
         Scanner sisendFailist = null;
         try {
             sisendFailist = new Scanner(fail);
@@ -23,18 +24,23 @@ public class test {
         // loeme failist
         while (sisendFailist.hasNextLine()) {
             String rida = sisendFailist.nextLine();
-            kontosisu.add(rida); // lisame loetud väärtus nimekirja sisse
+            opilased.add(rida); // lisame loetud väärtus nimekirja sisse
         }
-        System.out.println("Muusikapalade valik:");
-        int loop = 0;
-        while (loop < kontosisu.size()) {
-            System.out.println((loop + 1) + ". " + kontosisu.get(loop));
-            loop++;
-        }
-        System.out.println("Valige laulu järjekorranumber: ");
-        int number = sisesnd.nextInt();
-        System.out.println("Mängitav muusikapala on " + kontosisu.get(number - 1));
         sisendFailist.close();
+        // loome tänase kuupäeva
+        LocalDateTime tänaneKuupäev = LocalDateTime.now();
+        System.out.println("Tänane kuupäev ilma vorminduseta " + tänaneKuupäev);
+        DateTimeFormatter kuupäevaVormindus = DateTimeFormatter.ofPattern("dd");
+        String tänaneKuupäevVormindatud = tänaneKuupäev.format(kuupäevaVormindus);
+        System.out.println("Tänane vormindatud kuupäev " + tänaneKuupäevVormindatud);
+        /*// vaatame nimekirja sisu
+        for (int i = 0; i < opilased.size(); i++) {
+            System.out.println(i + " " + opilased.get(i));*/
+
+        // otsime vajalik inimene nimekirjast
+        int indeks = Integer.parseInt(tänaneKuupäevVormindatud);
+        System.out.println(opilased.get(indeks - 1));
+
     }
     }
 
