16f5b66a49e16bddecab65eb1454844fc8ada37d
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-15 14:00:15 +0200

    Konto programm töötab nüüd

--- src/test.java
+++ src/test.java
@@ -24,19 +24,20 @@ public class test {
         HashMap<String, Integer> konto = new HashMap<String, Integer>();
 
         // Kui tahab siis mis nimi
-        if ((String) konto() == "Jah") {
+        if (konto().equals("Jah")) {
             Scanner sisend = new Scanner(System.in);
             System.out.println("Sisestage kontonimi: ");
             String kontoNimi = sisend.nextLine();
             String kontonimi = kontoNimi.toUpperCase();
             konto.put(kontonimi, 0);
+            System.out.println(kontonimi + " kontol on " + konto.get(kontonimi));
         }
 
         // Kas kontolt raha sisse
         System.out.println("Kas tahad kontole raha panna (Jah/Ei)? ");
         Scanner kontoleRahawSisse = new Scanner(System.in);
         String kontoleRahawSisseNüüd = kontoleRahawSisse.nextLine();
-        if ((String) kontoleRahawSisseNüüd == "Jah") {
+        if (kontoleRahawSisseNüüd.equals("Jah")) {
             Scanner sisend = new Scanner(System.in);
             System.out.println("Sisestage kontonimi: ");
             String kontoNimi = sisend.nextLine();
@@ -53,7 +54,7 @@ public class test {
         System.out.println("Kas tahad kontolt raha võtta (Jah/Ei)? ");
         Scanner kontoleRahanüüd = new Scanner(System.in);
         String kontoleRahanüüd2 = kontoleRahanüüd.nextLine();
-        if ((String) kontoleRahanüüd2 == "Jah") {
+        if (kontoleRahanüüd2.equals("Jah")) {
             Scanner sisend = new Scanner(System.in);
             System.out.println("Sisestage kontonimi: ");
             String kontoNimi = sisend.nextLine();
