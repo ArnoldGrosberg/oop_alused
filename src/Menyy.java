c62663e9968fd9ad0007bbb294ac8b9a86a69778
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-22 13:24:59 +0200

    Ülesanne 3

---
+++ src/Menyy.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Menyy {
+
+    private ArrayList<String> soogid;
+
+    public Menyy() {
+        this.soogid = new ArrayList<String>();
+    }
+
+    public void lisaSook(String sook) {
+        soogid.add(sook);
+    }
+    // lisa meetodid siia
+}
+
