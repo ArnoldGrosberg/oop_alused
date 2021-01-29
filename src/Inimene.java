f5e2d004a0d10fb0eac60c763d3bc3f86851ee54
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-29 09:59:16 +0200

    Polymorphism inimese söömine 2

--- src/Inimene.java
+++ src/Inimene.java
@@ -32,21 +32,21 @@ public class Inimene {
         // kui kmi on 19 - 24.9 - see on norm
         // kui kmi on < 19 - tuleb inimest sööta
         // nii, et on ta võtaks 1 kg juurde  iga kord
-        while (kmi() < 19) {
-            suurendakaal();
+        // kui kmi on suurem siis peab lahudama
+        // ja kontrollida, kui on kmi nüüd korras
+        // juhul kui suurem kui 24.9 siis suurendan kaalu 1kg võrra
+        while (this.kmi() < 19.0) {
+            this.suurendakaal();
         }
 
-        // kui kmi on suurem siis peab lahudama
-
-        if (kmi() > 19 && kmi() < 24.9) {
+        if (this.kmi() >= 19.0 && this.kmi() < 25) {
             System.out.println("kmi on korras");
         }
-        // ja kontrollida, kui on kmi nüüd korras
 
-        // juhul kui suurem kui 24.9 siis suurendan kaalu 1kg võrra
         while (kmi() > 24.9) {
             suurendakaal(-1);
         }
+
         if (kmi() > 19 && kmi() < 24.9) {
             System.out.println("kmi on korras");
         }
