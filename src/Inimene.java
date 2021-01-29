03f46b5f16fef7d9d05984eca3727e7919e97f57
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-01-29 09:53:42 +0200

    Polymorphism inimese söömine

--- src/Inimene.java
+++ src/Inimene.java
@@ -1,10 +1,12 @@
 public class Inimene {
-    public String nimi;
-    private double pikkus = 0;
-    private double mass = 0;
+    private String nimi;
+    private double pikkus;
+    private double mass;
 
     public Inimene(String nimi) {
         this.nimi = nimi;
+        this.pikkus = 0.0;
+        this.mass = 0.0;
     }
 
     public String getNimi() {
@@ -13,8 +15,43 @@ public class Inimene {
 
 
     public double kmi() {
-        double kehamassiindeks = this.mass / (this.pikkus * this.pikkus);
+        double kehamassiindeks = this.mass / Math.pow(this.pikkus, 2);
         return kehamassiindeks;
+    }
+
+    // morphism
+    public void suurendakaal() {
+        this.mass++;
+    }
+
+    public void suurendakaal(double kg) {
+        this.mass += kg;
+    }
+
+    public void normaalkaal() {
+        // kui kmi on 19 - 24.9 - see on norm
+        // kui kmi on < 19 - tuleb inimest sööta
+        // nii, et on ta võtaks 1 kg juurde  iga kord
+        while (kmi() < 19) {
+            suurendakaal();
+        }
+
+        // kui kmi on suurem siis peab lahudama
+
+        if (kmi() > 19 && kmi() < 24.9) {
+            System.out.println("kmi on korras");
+        }
+        // ja kontrollida, kui on kmi nüüd korras
+
+        // juhul kui suurem kui 24.9 siis suurendan kaalu 1kg võrra
+        while (kmi() > 24.9) {
+            suurendakaal(-1);
+        }
+        if (kmi() > 19 && kmi() < 24.9) {
+            System.out.println("kmi on korras");
+        }
+
+
     }
 
     public double getPikkus() {
@@ -22,8 +59,7 @@ public class Inimene {
     }
 
     public void setPikkus(double pikkus) {
-        // pean jagama 100, sest muidu on liiga suured väärtused, meetrides peab olema
-        this.pikkus = pikkus / 100;
+        this.pikkus = pikkus;
     }
 
     public double getMass() {
--- src/Test.java
+++ src/Test.java
@@ -7,8 +7,29 @@ public class Test {
         Inimene matti = new Inimene("Matti");
         Inimene john = new Inimene("John");
 
-        matti.setPikkus(180);
-        matti.setMass(86);
+        matti.setPikkus(1.80);
+        matti.setMass(69.0);
+        System.out.println(matti.getNimi() + " on " + matti.getPikkus() + "m pikk ja kaalub " + matti.getMass() + " kg");
+        System.out.println("kmi = " + matti.kmi());
+        System.out.printf("kmi = %.2f\n", matti.kmi());
+
+        matti.suurendakaal();
+        System.out.println(matti.getNimi() + " on " + matti.getPikkus() + "m pikk ja kaalub " + matti.getMass() + " kg");
+        System.out.printf("kmi = %.2f\n", matti.kmi());
+
+        matti.suurendakaal(5.0);
+        System.out.println(matti.getNimi() + " on " + matti.getPikkus() + "m pikk ja kaalub " + matti.getMass() + " kg");
+        System.out.printf("kmi = %.2f\n", matti.kmi());
+
+        matti.suurendakaal(Math.random() * 100);
+        System.out.println(matti.getNimi() + " on " + matti.getPikkus() + "m pikk ja kaalub " + matti.getMass() + " kg");
+        System.out.printf("kmi = %.2f\n", matti.kmi());
+
+        matti.normaalkaal();
+
+        System.out.println(matti.getNimi() + " on " + matti.getPikkus() + "m pikk ja kaalub " + matti.getMass() + " kg");
+        System.out.printf("kmi = %.2f\n", matti.kmi());
+
 
         john.setPikkus(175);
         john.setMass(64);
