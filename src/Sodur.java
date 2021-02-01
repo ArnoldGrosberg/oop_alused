f78fd497118aa7073b8d99d5e783def3d372681d
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-02-01 13:43:08 +0200

    yl õp

--- src/Sodur.java
+++ src/Sodur.java
@@ -2,7 +2,6 @@ public class Sodur extends yl4_omaeesnimi_perenimi {
     private int armee_number;
 
     public void yl4_omaeesnimi_perenimi(String nimi, int id) {
-        super(nimi);
         this.armee_number = id;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        yl4_omaeesnimi_perenimi minuInimene = new yl4_omaeesnimi_perenimi();
-        yl4_omaeesnimi_perenimi.info();
+        yl4_omaeesnimi_perenimi inim = new yl4_omaeesnimi_perenimi();
+        yl4_omaeesnimi_perenimi inim2 = new yl4_omaeesnimi_perenimi();
     }
 }
