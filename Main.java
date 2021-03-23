bb004dcf9cf8a9eb78b0c43749d775c20df8dc0e
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-03-23 15:27:02 +0200

    6. StackPane

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Main extends Application{
     public static void main(String[] args){
@@ -13,19 +15,22 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) {
 
-        BorderPane root = new BorderPane();
+        Label headerLbl = new Label("Press the login button");
+        StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
+        StackPane.setMargin(headerLbl, new Insets(0,0,10,0));
 
-        Button top = new Button("Top");
-        BorderPane.setAlignment(top, Pos.CENTER);
-        root.setTop(top);
+        Label statusLbl = new Label("Start screen");
+        StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(statusLbl, new Insets(0,0,10,0));
 
-        Button center = new Button("Center");
-        root.setCenter(center);
+        Button loginBtn = new Button("Login");
+
+        StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
 
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("BorderPane in JavaFX");
+        stage.setTitle("StackPane in JavaFX");
 
         stage.show();
     }
