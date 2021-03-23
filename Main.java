dc4586cab72601cf7604d0f5180726c072f80d24
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-03-23 14:47:27 +0200

    1. Paigutuspaneelide tüübid

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 // import javafx.scene.control.Button;
 
@@ -11,16 +12,17 @@ public class Main extends Application{
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(root);
+        Label lbl = new Label("Hello");
+        Button btn = new Button("Click");
 
+        FlowPane root = new FlowPane();
+        root.getChildren().addAll(lbl, btn);
+        Scene scene = new Scene(root, 300, 200);
         stage.setScene(scene);
 
-        stage.setTitle("Hello JavaFX");
-        stage.setWidth(250);
-        stage.setHeight(200);
+        stage.setTitle("Layout in JavaFX");
 
         stage.show();
     }
