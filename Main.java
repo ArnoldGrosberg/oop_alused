a90100720f79a1282155d6dfc8a7dcdeb47f5607
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-03-23 14:59:43 +0200

    3. HBox

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
-// import javafx.scene.control.Button;
+import javafx.scene.control.Button;
 
 public class Main extends Application{
     public static void main(String[] args){
@@ -14,15 +13,18 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) {
 
-        Label label1 = new Label("Label1");
-        Label label2 = new Label("Label2");
-
-        FlowPane root = new FlowPane(10,10,label1,label2);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 300, 150);
+        HBox hbox = new HBox();
+        Button button1 = new Button("Add");
+        Button button2 = new Button("Remove");
+        HBox.setHgrow(button1, Priority.ALWAYS);
+        HBox.setHgrow(button2, Priority.ALWAYS);
+        button1.setMaxWidth(Double.MAX_VALUE);
+        button2.setMaxWidth(Double.MAX_VALUE);
+        hbox.getChildren().addAll(button1,button2);
+        Scene scene = new Scene(hbox, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("FlowPane in JavaFX");
+        stage.setTitle("HBox in JavaFX");
 
         stage.show();
     }
