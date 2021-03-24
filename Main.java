156ae9b8a4e80efc72334e33c6577da3f62f9e56
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-03-24 10:52:13 +0200

    11. TreeView

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
@@ -16,20 +19,49 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception{
 
-        ObservableList<String> langs = FXCollections.observableArrayList("Java","JavaScript", "C#","Python");
-        ChoiceBox<String> langsChoiceBox = new ChoiceBox<String>(langs);
+        TreeItem<String> rootTreeNode = new TreeItem<String>("Languages");
+        rootTreeNode.setExpanded(true); // raskrõvaem uzel
 
-        langsChoiceBox.setValue("Java"); // määrame vaikimisi valitud element
+        TreeItem<String> germanics = new TreeItem<String>("Germanic");
+        germanics.getChildren().add(new TreeItem<String>("German"));
+        germanics.getChildren().add(new TreeItem<String>("English"));
+
+        TreeItem<String> romans = new TreeItem<String>("Roman");
+        romans.getChildren().add(new TreeItem<String>("Frence"));
+        romans.getChildren().add(new TreeItem<String>("Spanish"));
+        romans.getChildren().add(new TreeItem<String>("Italian"));
+
+        rootTreeNode.getChildren().add(germanics);
+        rootTreeNode.getChildren().add(romans);
+
+        TreeView<String> langsTreeView = new TreeView<String>(rootTreeNode);
+        langsTreeView.setPrefSize(150,200);
+
+        MultipleSelectionModel<TreeItem<String>> selectionModel = langsTreeView.getSelectionModel();
 
         Label lbl = new Label();
-        // saame valitud element
-        langsChoiceBox.setOnAction(event -> lbl.setText(langsChoiceBox.getValue()));
 
-        FlowPane root = new FlowPane(10,10, langsChoiceBox, lbl);
+        selectionModel.selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>() {
+
+            public void changed(ObservableValue<? extends TreeItem<String>> changed, TreeItem<String> oldValue, TreeItem<String> newValue) {
+                if(newValue != null){
+                    String path = newValue.getValue();
+                    TreeItem<String> parent = newValue.getParent();
+                    while(parent != null){
+                        path = parent.getValue() + "/" + path;
+                        parent = parent.getParent();
+                    }
+                    lbl.setText(path);
+                }
+
+            }
+        });
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL,10,10, langsTreeView, lbl);
         Scene scene = new Scene(root,300,250);
 
         stage.setScene(scene);
-        stage.setTitle("ChoiceBox in JavaFX");
+        stage.setTitle("TreeView in JavaFX");
         stage.show();
     }
 }
